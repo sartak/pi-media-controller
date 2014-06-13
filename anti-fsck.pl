821a36a65c9a421fa85b246a5cdd527bdcd8a8e4
Shawn M Moore <shawn.moore@iinteractive.com>
2014-06-13 03:35:44 -0400

    anti-fsck

---
+++ anti-fsck.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+use 5.14.0;
+use warnings;
+use utf8::all;
+use Pi::Media::Library;
+use File::Next;
+use Path::Class 'file';
+
+my $library = Pi::Media::Library->new;
+my %seen = map { $_ => 1 } $library->paths;
+my $iterator = File::Next::files(@ARGV ? @ARGV : '.');
+while (defined(my $file = $iterator->())) {
+    my $full = file($file)->absolute;
+    next if $seen{$full};
+    warn "$full\n";
+}
