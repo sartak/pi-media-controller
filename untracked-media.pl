bb07e9af70302fae1e1feb578b0846b17e276695
Shawn M Moore <shawn.moore@iinteractive.com>
2014-07-14 00:38:15 -0400

    Switch to File::Find since File::Next screws up

--- untracked-media.pl
+++ untracked-media.pl
@@ -3,16 +3,20 @@ use 5.14.0;
 use warnings;
 use utf8::all;
 use Pi::Media::Library;
-use File::Next;
 use Path::Class 'file';
+use File::Find;
 
 my $library = Pi::Media::Library->new(file => $ENV{PMC_DATABASE});
 my %seen = map { $_ => 1 } $library->paths;
-my $iterator = File::Next::files(@ARGV ? @ARGV : '.');
-while (defined(my $file = $iterator->())) {
-    next if $file =~ /\.DS_Store/;
 
-    my $full = file($file)->absolute;
-    next if $seen{$full};
-    warn "$full\n";
-}
+@ARGV or die "usage: $0 directories\n";
+
+find(sub {
+    return if -d $_;
+
+    my $file = $File::Find::name;
+    return if $file =~ /\.DS_Store/;
+    return if $seen{$file};
+
+    warn $file;
+}, @ARGV);
