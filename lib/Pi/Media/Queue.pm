c5064599893dc7fb0daa220cdf237763179cca5f
Shawn M Moore <shawn.moore@iinteractive.com>
2014-08-09 17:05:36 -0400

    Better queue ids

--- lib/Pi/Media/Queue.pm
+++ lib/Pi/Media/Queue.pm
@@ -29,7 +29,7 @@ sub push {
     for my $original (@_) {
         my $copy = \%$original;
 
-        $copy->{queue_id} = $Serial++;
+        $copy->{queue_id} = $$ . "-" . $Serial++;
         push @{ $self->{_videos} }, $copy;
     }
 }
