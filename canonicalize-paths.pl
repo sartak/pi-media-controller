c853f77a3772a4eb7dc316385cfcf4e4fb64c99a
Shawn M Moore <shawn.moore@iinteractive.com>
2015-04-27 11:41:10 -0400

    canonicalize-paths script

---
+++ canonicalize-paths.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl
+use 5.14.0;
+use warnings;
+use utf8::all;
+use Pi::Media::Library;
+use Unicode::Normalize 'normalize';
+
+my $form = shift or die "usage: $0 [form]";
+
+my $library = Pi::Media::Library->new(file => $ENV{PMC_DATABASE});
+
+for my $media ($library->media(all => 1, excludeViewing => 1)) {
+    my $path = $media->_relativify_path;
+    my $normalized = normalize($form, $path);
+    if ($normalized ne $path) {
+        $library->update_media($media, (
+            path => $normalized,
+        ));
+    }
+}
+
