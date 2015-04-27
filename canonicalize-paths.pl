149bc3c7d36bc9d6f4f049a16494bed065400375
Shawn M Moore <shawn.moore@iinteractive.com>
2015-04-27 15:22:55 -0400

    Fix method call

--- canonicalize-paths.pl
+++ canonicalize-paths.pl
@@ -10,7 +10,7 @@ my $form = shift or die "usage: $0 [form]";
 my $library = Pi::Media::Library->new(file => $ENV{PMC_DATABASE});
 
 for my $media ($library->media(all => 1, excludeViewing => 1)) {
-    my $path = $media->_relativify_path;
+    my $path = $library->_relativify_path($media->path);
     my $normalized = normalize($form, $path);
     if ($normalized ne $path) {
         $library->update_media($media, (
