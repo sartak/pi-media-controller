d8922db700115f33e12dc9caffe4921b5eb20867
Shawn M Moore <shawn@bestpractical.com>
2015-05-16 19:48:07 -0400

    Require config for gamepads

--- lib/Pi/Media/Gamepad.pm
+++ lib/Pi/Media/Gamepad.pm
@@ -2,6 +2,12 @@ package Pi::Media::Gamepad;
 use 5.14.0;
 use Mouse;
 
+has config => (
+    is       => 'ro',
+    isa      => 'HashRef',
+    required => 1,
+);
+
 has led => (
     is  => 'ro',
     isa => 'Int',
