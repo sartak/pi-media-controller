9efbe2d21ca4a594156c4c387e313bc22d1a2e3c
Shawn M Moore <shawn@bestpractical.com>
2015-09-27 01:23:42 -0400

    fix

--- intuit-checksums.pl
+++ intuit-checksums.pl
@@ -22,7 +22,7 @@ for my $media (@media) {
     $sha->addfile($handle);
     my $checksum = lc($sha->hexdigest);
 
-    my @dupes = $library->media(checksum => $checksum);
+    my @dupes = $library->media(all => 1, checksum => $checksum);
 
     $library->update_media($media, (
         checksum => $checksum,
