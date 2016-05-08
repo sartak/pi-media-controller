7c0e7cbdd3101533225320396360fe95a6eaaeea
Shawn M Moore <code@sartak.org>
2016-05-08 20:26:19 +0000

    Have add-tree-media-sort just fix all the trees

--- add-tree-media-sort.pl
+++ add-tree-media-sort.pl
@@ -6,36 +6,34 @@ use JSON;
 use Getopt::Whatever;
 use Pi::Media::Library;
 
-die "usage: $0 treeId or segments required\n" if !@ARGV;
+die "usage: $0\n" if @ARGV;
 my $library = Pi::Media::Library->new(file => $ENV{PMC_DATABASE});
+my @trees = $library->trees(all => 1, media_sort => 1);
 
-my $treeId;
-if (@ARGV == 1 && $ARGV[0] =~ /^\d+$/) {
-    $treeId = shift;
-}
-else {
-    $treeId = $library->tree_from_segments(@ARGV);
-}
+for my $tree (@trees) {
+    next unless $tree->has_clause;
 
-$library->begin;
-my ($tree) = $library->trees(id => $treeId);
-if (!$tree->query) {
-    die "tree $treeId has no where clause";
-}
+    $library->begin;
+    my @media = $library->media(
+        all         => 1,
+        joins       => $tree->join_clause,
+        where       => $tree->where_clause,
+        order       => $tree->order_clause,
+        limit       => $tree->limit_clause,
+        source_tree => $tree->id,
+    );
 
-my @media = $library->media(where => $tree->query);
-
-my $sth = $library->_dbh->prepare("INSERT OR IGNORE INTO tree_media_sort (mediaId, treeId, identifier, sort_order) VALUES (?, ?, ?, ?);");
-
-my $i = 0;
-for my $media (@media) {
-    # no way we can guess something useful, so at least make it easy to manage
-    my $identifier = $media->label->{en} || $media->label->{ja};
-    my $result = $sth->execute($media->id, $treeId, $identifier, ++$i);
-    if ($result > 0) {
-        print $media->id . " ($i): $identifier\n";
+    my $sth = $library->_dbh->prepare("INSERT OR IGNORE INTO tree_media_sort (mediaId, treeId, identifier, sort_order) VALUES (?, ?, ?, ?);");
+    
+    my $i = 0;
+    for my $media (@media) {
+        # no way we can guess something useful, so at least make it easy to manage
+        my $identifier = $media->label->{en} || $media->label->{ja};
+        my $result = $sth->execute($media->id, $tree->id, $identifier, ++$i);
+        if ($result > 0) {
+            print(($tree->label->{ja} || $tree->label->{en}) . " " . $media->id . " ($i): $identifier\n");
+        }
     }
+
+    $library->commit;
 }
-
-$library->commit;
-
