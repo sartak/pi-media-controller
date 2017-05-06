87342b245a84b0ac388e58e76130174e7eb650e9
Shawn M Moore <shawn@bestpractical.com>
2017-05-06 19:41:36 +0000

    Tidy

--- janitor.sh
+++ janitor.sh
@@ -17,8 +17,8 @@ perl -Ilib -Iextlib intuit-durations.pl
 
 perl -Ilib -Iextlib track-counts.pl
 
-perl -Ilib -Iextlib ./add-tree-media-sort.pl
+perl -Ilib -Iextlib add-tree-media-sort.pl
 
-perl -Ilib -Iextlib intuit-checksums.pl;
+perl -Ilib -Iextlib intuit-checksums.pl
 
 echo 'select path, spoken_langs from media where spoken_langs like "%?%" and treeId=1;' | sqlite3 $PMC_DATABASE
